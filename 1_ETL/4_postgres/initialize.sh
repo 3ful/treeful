fb7b752dd4a71c092f38f19c38d1794a71c348bf
chris-toph-t <admin@toph.eu>
2023-06-17 20:11:37 +0000

    beef up postgres init script

--- 1_ETL/4_postgres/initialize.sh
+++ 1_ETL/4_postgres/initialize.sh
@@ -1,3 +1,15 @@
-psql -c 'CREATE EXTENSION postgis_raster;'
-psql -c 'SET postgis.gdal_enabled_drivers = 'ENABLE_ALL';'
-psql -c "SELECT name, default_version,installed_version FROM pg_available_extensions WHERE name LIKE 'postgis%' or name LIKE 'address%';"
+#!/bin/bash
+
+set -e
+
+# Perform all actions as $POSTGRES_USER
+export PGUSER="$POSTGRES_USER"
+
+# Load PostGIS into both template_database and $POSTGRES_DB
+
+echo "Loading PostGIS extensions into $DB"
+"${psql[@]}" --dbname="treeful-test" <<-'EOSQL'
+	CREATE EXTENSION postgis_raster;
+	SET postgis.gdal_enabled_drivers = 'ENABLE_ALL';
+	SELECT name, default_version,installed_version FROM pg_available_extensions WHERE name LIKE 'postgis%' or name LIKE 'address%';
+EOSQL
