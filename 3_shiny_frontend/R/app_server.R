d5a8c45a9a6e18b28ef894d2dd7c1219032d043d
chris-toph-t <admin@toph.eu>
2023-07-18 21:41:49 +0000

    fix db connect external pointer error

--- 3_shiny_frontend/R/app_server.R
+++ 3_shiny_frontend/R/app_server.R
@@ -30,13 +30,13 @@ app_server <- function(input, output, session) {
   username <- Sys.getenv("SHINYPROXY_USERNAME")
   Sys.setlocale("LC_TIME","de_DE.UTF-8")
 
-  conn <- DBI::dbConnect(RPostgres::Postgres(),
+  backend_con <- DBI::dbConnect(RPostgres::Postgres(),
                         dbname = "treeful-test",
                         host= "192.168.178.148",
                         port="5432",
                         user="postgres",
                         password="mysecretpassword")
-  on.exit(DBI::dbDisconnect(con), add = TRUE)
+  #on.exit(DBI::dbDisconnect(backend_con), add = TRUE)
 
   output$map <- renderLeaflet({leaflet()%>% addTiles()})
 
@@ -48,45 +48,6 @@ app_server <- function(input, output, session) {
 
   })
 
-  ##################### Function to Get past climate ##################
-  getpastclimate <- function(source = "copernicus", bioclim = "bio01") {
-    if (source == "copernicus") {
-      # Get bioclimate data from copernicus. Download bioclimate file with login at https://cds.climate.copernicus.eu/
-
-      bio_path <- dplyr::case_when(bioclim == "bio01" ~ "BIO01",
-                            bioclim == "bio12" ~ "BIO12"
-      )
-
-      bio_raster <- raster::raster(paste0("data-raw/copernicus/", bio_path, "_era5-to-1km_1979-2018-mean_v1.0.nc"))
-      # convert bioclim as per copernicus documentation. for some reasone case_when does not work here.
-      if (bioclim == "bio01") {bio_raster <- raster::calc(bio_raster, function(x) {x - 273.15})
-      } else if (bioclim == "bio12") {bio_raster <- raster::calc(bio_raster, function(x) {x*3600*24*365*1000})}
-    }
-    return(bio_raster)
-  }
-
-
-  #################### Get Future Climate ##################
-  # for now using climate projection model MPI-ESM1-2-LR and socio-econ pathway 245
-  getfutureclimate <- function(source = "copernicus") {
-    if (source == "copernicus") {
-      future_raster_bio01 <- raster::stack("data-raw/copernicus/future/BIO01_hadgem2-cc_rcp45_r1i1p1_1960-2099-mean_v1.0.nc")$X2050.01.01
-      future_raster_bio12 <- raster::stack("data-raw/copernicus/future/BIO12_hadgem2-cc_rcp45_r1i1p1_1960-2099-mean_v1.0.nc")$X2050.01.01
-
-      future_raster_bio01 <- raster::calc(future_raster_bio01, function(x) {x - 273.15})
-      future_raster_bio12 <- raster::calc(future_raster_bio12, function(x) {x*3600*24*365*1000})
-
-      future_raster <- raster::stack(future_raster_bio01, future_raster_bio12)
-      names(future_raster)[1] <- "bio01"
-      names(future_raster)[2] <- "bio12"
-    }
-    return(future_raster)
-  }
-
-
-
-  #future_raster <- getfutureclimate(source = "copernicus")
-
   make_query <- function(map_point, layer = "", band = 1) {
     return(paste0("SELECT g.pt_geom, ST_Value(ST_Band(r.rast, ARRAY[", band, "]), g.pt_geom) AS biovar
       FROM public.", layer, " AS r
@@ -95,16 +56,16 @@ app_server <- function(input, output, session) {
       ON r.rast && g.pt_geom;"))
   }
 
-  get_user_climate <- function(con = conn, lat = input$map_click$lat, lon = input$map_click$lng) {
+  get_user_climate <- function(connection = backend_con, lat = input$map_click$lat, lon = input$map_click$lng) {
     map_point <- sf::st_as_sf(tibble(lat = lat, lon = lon), coords = c("lon", "lat"), crs = 4326, remove =FALSE)
 
     #get past at map location
-    bio01_hist <- RPostgreSQL::dbGetQuery(con,make_query(map_point, layer = "pastbio01", band = 1))$biovar
-    bio12_hist <- RPostgreSQL::dbGetQuery(con,make_query(map_point, layer = "pastbio12", band = 1))$biovar
+    bio01_hist <- RPostgreSQL::dbGetQuery(connection,make_query(map_point, layer = "pastbio01", band = 1))$biovar
+    bio12_hist <- RPostgreSQL::dbGetQuery(connection,make_query(map_point, layer = "pastbio12", band = 1))$biovar
 
 
-    bio01_future <- RPostgreSQL::dbGetQuery(con,make_query(map_point, layer = "future", band = 1))$biovar
-    bio12_future <-  RPostgreSQL::dbGetQuery(con,make_query(map_point, layer = "future", band = 2))$biovar
+    bio01_future <- RPostgreSQL::dbGetQuery(connection,make_query(map_point, layer = "future", band = 1))$biovar
+    bio12_future <-  RPostgreSQL::dbGetQuery(connection,make_query(map_point, layer = "future", band = 2))$biovar
 
     return(tibble(bio01_future, bio12_future,
                   bio01_hist, bio12_hist
@@ -113,22 +74,21 @@ app_server <- function(input, output, session) {
   user_climate1 <- reactive({
     req(input$map_click)
     get_user_climate()
-
     })
 
   output$user_location <- renderDT(user_climate1())
 
-  tree_db <- data.table::fread("data/tree_db.csv")
-  species <- unique(tree_db$master_list_name)
+  #tree_db <- data.table::fread("data/tree_db.csv")
+  species <- DBI::dbGetQuery(backend_con, paste0("SELECT DISTINCT master_list_name FROM tree_dbs"))
 
-  updateSelectInput(session, "select_species", choices = species, selected = "Sorbus torminalis")
+  updateSelectInput(session, "select_species", choices = species$master_list_name, selected = "Sorbus torminalis")
 
   # tree_occurrence <- reactive(tree_db[master_list_name %in% c(input$select_species), ]
   # )
   #selection <- reactive(input$select_species)
 
   tree_occurrence <- reactive(
-    DBI::dbGetQuery(conn, paste0(
+    DBI::dbGetQuery(backend_con, paste0(
       "SELECT * FROM tree_dbs WHERE master_list_name ='", input$select_species, "';"))
   )
 
@@ -154,7 +114,7 @@ app_server <- function(input, output, session) {
     scale_fill_continuous(type = "viridis") +
     ggplot2::geom_point(data = user_climate1(), aes(x = bio12_hist, y = bio01_hist), color = "darkolivegreen4", size = 4) +
     ggplot2::geom_point(data = user_climate1(), aes(x = bio12_future, y = bio01_future), color = "darkolivegreen", size = 4) +
-    scale_color_paletteer_d("wesanderson::Royal1") +
+    #scale_color_paletteer_d("wesanderson::Royal1") +
     ggplot2::facet_wrap(~master_list_name) +
     hrbrthemes::theme_ipsum() +
     ggplot2::labs(title = paste0("Jahrestemperatur und Jahresniederschlag"),
--- 3_shiny_frontend/inst/app/www/intro.md
+++ 3_shiny_frontend/inst/app/www/intro.md
@@ -3,9 +3,7 @@
 Such dir auf der Karte einen Standort aus. Wir zeigen dir wie das Klima dort war und wird. 
 Im nächsten Schritt siehst du das Habitat verschiedener Baumarten. 
 
-## Stand der Enticklung
+## Ergebnisse sind nur so genau wie Klimavorhersagen
 
-We only allow the selection of bio1 and bio12. We only tak historic and future data as per copernicus. 
-Future data is from UK Met bureau climate model. Somehow they seemed trustworthy. 
+Wir nutzen Klimavorhersagen von Copernicus. Das bedeutet: Copernicus versucht europaweit vorherzusagen, welche Temperatur, Niederschlag, etc an einem Standort herrschen. Diese Vorhersagen können natürlich ungenau sein, sind jedoch das beste was momentan verfügbar ist. 
 
-Deployment still unclear how to proceed with large file dependencies. They cannot be packaged as package, >10GB. Need to be manually synced or fetched in Dockerfile. 
