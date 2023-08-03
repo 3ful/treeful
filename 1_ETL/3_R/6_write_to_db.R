1b8a4aa922cf5d273bcd9a5b2dd6fd7742e732eb
chris-toph-t <admin@toph.eu>
2023-08-03 21:37:58 +0000

    write rasters as stack to postgres, not terra compatible...

--- 1_ETL/3_R/6_write_to_db.R
+++ 1_ETL/3_R/6_write_to_db.R
@@ -1,3 +1,4 @@
+print("large data transfer out starting. Writing all Rasters as rasterstack to Postgres")
 
 con <- DBI::dbConnect(RPostgres::Postgres(), 
                       dbname = Sys.getenv("POSTGRES_DB"),
@@ -5,67 +6,67 @@ con <- DBI::dbConnect(RPostgres::Postgres(),
                       port="5432",
                       user="postgres",
                       password=Sys.getenv("POSTGRES_PW"))
-
-future <- c(getfutureclimate(source = "copernicus", bioclim = "bio01"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio02"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio03"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio04"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio05"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio06"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio07"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio08"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio09"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio10"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio11"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio12"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio13"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio14"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio15"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio16"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio17"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio18"),
-                        getfutureclimate(source = "copernicus", bioclim = "bio19")
+print("starting with reading Copernicus Future")
+future <- raster::stack(raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio01")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio02")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio03")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio04")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio05")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio06")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio07")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio08")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio09")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio10")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio11")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio12")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio13")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio14")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio15")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio16")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio17")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio18")),
+                        raster::raster(getfutureclimate(source = "copernicus", bioclim = "bio19"))
                         )
 
-
-past <- c(getpastclimate(source = "copernicus", bioclim = "bio01"),
-                        getpastclimate(source = "copernicus", bioclim = "bio02"),
-                        getpastclimate(source = "copernicus", bioclim = "bio03"),
-                        getpastclimate(source = "copernicus", bioclim = "bio04"),
-                        getpastclimate(source = "copernicus", bioclim = "bio05"),
-                        getpastclimate(source = "copernicus", bioclim = "bio06"),
-                        getpastclimate(source = "copernicus", bioclim = "bio07"),
-                        getpastclimate(source = "copernicus", bioclim = "bio08"),
-                        getpastclimate(source = "copernicus", bioclim = "bio09"),
-                        getpastclimate(source = "copernicus", bioclim = "bio10"),
-                        getpastclimate(source = "copernicus", bioclim = "bio11"),
-                        getpastclimate(source = "copernicus", bioclim = "bio12"),
-                        getpastclimate(source = "copernicus", bioclim = "bio13"),
-                        getpastclimate(source = "copernicus", bioclim = "bio14"),
-                        getpastclimate(source = "copernicus", bioclim = "bio15"),
-                        getpastclimate(source = "copernicus", bioclim = "bio16"),
-                        getpastclimate(source = "copernicus", bioclim = "bio17"),
-                        getpastclimate(source = "copernicus", bioclim = "bio18"),
-                        getpastclimate(source = "copernicus", bioclim = "bio19")
+print("Reading in Copernicus past raster")
+past <- raster::stack(raster::raster(getpastclimate(source = "copernicus", bioclim = "bio01")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio02")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio03")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio04")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio05")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio06")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio07")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio08")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio09")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio10")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio11")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio12")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio13")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio14")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio15")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio16")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio17")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio18")),
+                        raster::raster(getpastclimate(source = "copernicus", bioclim = "bio19"))
 )
-
-soil <- c(getsoilproperties("STU_EU_DEPTH_ROOTS"),
-                  getsoilproperties("STU_EU_T_CLAY"),
-                  getsoilproperties("STU_EU_S_CLAY"),
-                  getsoilproperties("STU_EU_T_SAND"),
-                  getsoilproperties("STU_EU_S_SAND"),
-                  getsoilproperties("STU_EU_T_SILT"),
-                  getsoilproperties("STU_EU_S_SILT"),
-                  getsoilproperties("STU_EU_T_OC"),
-                  getsoilproperties("STU_EU_S_OC"),
-                  getsoilproperties("STU_EU_T_BD"),
-                  getsoilproperties("STU_EU_S_BD"),
-                  getsoilproperties("STU_EU_T_GRAVEL"),
-                  getsoilproperties("STU_EU_S_GRAVEL"),
-                  getsoilproperties("SMU_EU_T_TAWC"),
-                  getsoilproperties("SMU_EU_S_TAWC"),
-                  getsoilproperties("STU_EU_T_TAWC"),
-                  getsoilproperties("STU_EU_S_TAWC")
+print("Reading in Soil Rasters")
+soil <- raster::stack(raster::raster(getsoilproperties("STU_EU_DEPTH_ROOTS")),
+                  raster::raster(getsoilproperties("STU_EU_T_CLAY")),
+                  raster::raster(getsoilproperties("STU_EU_S_CLAY")),
+                  raster::raster(getsoilproperties("STU_EU_T_SAND")),
+                  raster::raster(getsoilproperties("STU_EU_S_SAND")),
+                  raster::raster(getsoilproperties("STU_EU_T_SILT")),
+                  raster::raster(getsoilproperties("STU_EU_S_SILT")),
+                  raster::raster(getsoilproperties("STU_EU_T_OC")),
+                  raster::raster(getsoilproperties("STU_EU_S_OC")),
+                  raster::raster(getsoilproperties("STU_EU_T_BD")),
+                  raster::raster(getsoilproperties("STU_EU_S_BD")),
+                  raster::raster(getsoilproperties("STU_EU_T_GRAVEL")),
+                  raster::raster(getsoilproperties("STU_EU_S_GRAVEL")),
+                  raster::raster(getsoilproperties("SMU_EU_T_TAWC")),
+                  raster::raster(getsoilproperties("SMU_EU_S_TAWC")),
+                  raster::raster(getsoilproperties("STU_EU_T_TAWC")),
+                  raster::raster(getsoilproperties("STU_EU_S_TAWC"))
           )
 
 # 
@@ -77,13 +78,15 @@ soil <- c(getsoilproperties("STU_EU_DEPTH_ROOTS"),
 # # we set this proj here and it seems to stick to R raster object. 
 # # when writing into postgis, there SRID appears to be 3395
 
-
+print("Writing Copernicus Past to DB")
 rpostgis::pgWriteRast(con,
             name = "past", raster = past, overwrite = TRUE
 )
+print("Writing Copernicus Future to DB")
 rpostgis::pgWriteRast(con,
             name = "future", raster = future, overwrite = TRUE
 )
+print("Writing Soil to DB")
 rpostgis::pgWriteRast(con,
                       name = "soil", raster = soil, overwrite = TRUE
 )
