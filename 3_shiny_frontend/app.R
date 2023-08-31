5ba502663bdd83f651f5f7cda20b3e8748f14b59
chris-toph-t <admin@toph.eu>
2023-08-31 12:37:13 +0000

    add tree profile mount in compose

--- 3_shiny_frontend/app.R
+++ 3_shiny_frontend/app.R
@@ -2,7 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-#library(treeful)
+#pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+library(treeful)
 options( "golem.app.prod" = TRUE)
 treeful::run_app() # add parameters here (if any)
